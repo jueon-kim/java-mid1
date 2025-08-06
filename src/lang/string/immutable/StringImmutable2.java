9931edb25c216cf5fa57e30bc1cfc2bef96463e1
ice-bear <jueomgim202@gmail.com>
2025-08-06 22:53:54 +0900

    immutable따로 관리

--- src/lang/string/immutable/StringImmutable2.java
+++ src/lang/string/immutable/StringImmutable2.java
@@ -1,11 +1,12 @@
 package lang.string.immutable;
 
-public class StringImmutable1 {
+public class StringImmutable2 {
     public static void main(String[] args) {
 
-        String str = "hello";
-        str.concat("java");
-        System.out.println("str = " + str);
+        String str1 = "hello";
+        String str2 = str1.concat("java");
+        System.out.println("str1 = " + str1);
+        System.out.println("str2 = " + str2);
         
     }
 }
