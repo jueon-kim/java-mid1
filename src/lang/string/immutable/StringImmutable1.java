b8ba518a8e46650c6c921664a89e4c7cbe9c06b6
ice-bear <jueomgim202@gmail.com>
2025-08-06 23:39:36 +0900

    Immutable 다시 푸쉬

--- src/lang/string/immutable/StringImmutable1.java
+++ src/lang/string/immutable/StringImmutable1.java
@@ -1,19 +1,10 @@
 package lang.string.immutable;
 
 public class StringImmutable1 {
-<<<<<<< HEAD
 
     public static void main(String[] args) {
         String str = "hello";
         str.concat(" java");
         System.out.println("str = " + str);
-=======
-    public static void main(String[] args) {
-
-        String str = "hello";
-        str.concat("java");
-        System.out.println("str = " + str);
-
->>>>>>> immutable
     }
 }
